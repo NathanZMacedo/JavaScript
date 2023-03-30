b6201016c20b0c2fbc836f171a15f01a497a0599
NathanZMacedo <NathanZMacedo@gmail.com>
2023-03-30 17:21:22 -0300

    comparações das variaveis

---
+++ Nathan-Quinta16H/JavaScript/comparacao.js
@@ -0,0 +1,9 @@
+// um igual significa atribuição
+var numero = 12;
+const texto = "12"
+// dois iguais é uma pergunta sem tipagem
+console.log(texto == numero);
+// tres iguais verifica se oconteudo é o mesmo e também checa a tipagem
+console.log(texto === numero);
+// dois iguais e uma esclamação checam se é diferente
+console.log(texto !== numero);
