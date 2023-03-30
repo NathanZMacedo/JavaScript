5877a174a7cb392960aa62f745d217797191067d
NathanZMacedo <NathanZMacedo@gmail.com>
2023-03-30 17:10:20 -0300

    tipos de variavel

---
+++ Nathan-Quinta16H/JavaScript/variavel.js
@@ -0,0 +1,9 @@
+var variavel = 12;
+const nome = "nome de alguem";
+// a varoavel do tipo let só existe dentro da função somar
+function somar(numero){
+    let futuro = 10;
+    console.log("no futuro voce terá ",futuro + numero," anos");
+}
+//somar (21);
+//console.log(futuro);
